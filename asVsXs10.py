1b475683eae4d230aeb2e6c54fee6d91d7a06853
cbboipdx@gmail.com <fl9788@commodore.grid.wayne.edu>
2014-01-16 15:32:44 -0500

    compare mappability

---
+++ asVsXs10.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+## filter  reads lacking sufficient
+## differences in mappability to multiple positions in the genome
+import sys
+
+#line = sys.stdin.readlines()
+thresh = 10
+
+for ii in sys.stdin: 
+	
+	if ii[0] == "@":
+		print ii.strip()	
+	else:
+		aStart = ii.find("AS:i:")
+		asNum = (ii[aStart+5:aStart+7]).strip()
+		
+		xStart = ii.find("XS:i")
+		xsNum = (ii[xStart+5:xStart+7]).strip()
+
+		if abs(int(asNum)-int(xsNum)) >= thresh:
+			print ii.strip()
